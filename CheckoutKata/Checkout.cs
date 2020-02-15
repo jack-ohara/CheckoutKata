bc0c37947003af0a32c406d300ff63e22b880714
Jack O'Hara <jack.ohara@sorted.com>
2020-02-15 15:33:25 +0000

    Added a new test for Item, as we will need to expose the SKU, which should not be null, empty or whitespace

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -5,10 +5,12 @@ namespace CheckoutKata
 {
     public class Checkout
     {
+        private readonly HashSet<SpecialOffer> _specialOffers;
         private readonly List<Item> _scannedItems;
 
         public Checkout(HashSet<SpecialOffer> specialOffers)
         {
+            _specialOffers = specialOffers;
             _scannedItems = new List<Item>();
         }
 
@@ -19,7 +21,9 @@ namespace CheckoutKata
 
         public int GetTotalPrice()
         {
-            return _scannedItems.Sum(item => item.Price);
+            return _specialOffers
+                       .FirstOrDefault(offer => _scannedItems.Select(item => item.Sku).Contains(offer.ItemSku))
+                       .SpecialPrice ?? _scannedItems.Sum(item => item.Price);
         }
     }
 }
---
+++ CheckoutKataTests/ItemTests.cs
@@ -0,0 +1,18 @@
+﻿using CheckoutKata;
+using NUnit.Framework;
+
+namespace CheckoutKataTests
+{
+    [TestFixture]
+    public class ItemTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("      ")]
+        public void ThrowsAnErrorIfProvidedWithANullOrEmptySkuValue(string sku)
+        {
+            Assert.That(() => new Item(sku, 0),
+                Throws.ArgumentException.With.Message.EqualTo("A SKU must be provided for an item"));
+        }
+    }
+}

3eca5607a8985b39eab97f67aa7657bd0398dbe1
Jack O'Hara <jack.ohara@sorted.com>
2020-02-15 14:42:22 +0000

    Test is now green with minimum possible implementation

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -4,12 +4,11 @@
     {
         public void Scan(Item item)
         {
-            throw new System.NotImplementedException();
         }
 
         public int GetTotalPrice()
         {
-            throw new System.NotImplementedException();
+            return 50;
         }
     }
 }

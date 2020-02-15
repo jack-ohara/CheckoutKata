00919a3d5745270128da01e732c789a5b5deaeae
Jack O'Hara <jack.ohara@sorted.com>
2020-02-15 14:37:51 +0000

    Added the first failing test to check that the checkout returns the price of a single scanned item. Currently doesn't compile as the Checkout and Item classes do not exist

---
+++ CheckoutKataTests/CheckoutTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKataTests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        private Checkout _checkout;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _checkout = new Checkout();
+        }
+
+        [Test]
+        public void ReturnsThePriceOfASingleScannedItem()
+        {
+            var item = new Item("A", 50);
+
+            _checkout.Scan(item);
+
+            var totalPrice = _checkout.GetTotalPrice();
+
+            Assert.That(totalPrice, Is.EqualTo(item.Price));
+        }
+    }
+}
